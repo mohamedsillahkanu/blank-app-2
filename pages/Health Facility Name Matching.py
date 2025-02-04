8c8798dd0a02e140fe9b2394bfefc8cdcd65a79b
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-02-04 15:57:12 -0600

    Update Health Facility Name Matching.py

--- pages/Health Facility Name Matching.py
+++ pages/Health Facility Name Matching.py
@@ -39,7 +39,7 @@ st.markdown("""
     }
     .uploadedFile {
         margin-bottom: 1rem;
-        background: gray;
+        background: #FFFFFF;
         padding: 1rem;
         border-radius: 4px;
         border: 1px solid #1E88E5;
