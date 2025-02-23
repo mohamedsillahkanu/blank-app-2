7fb8cf9416788caf78f297dc722f2573b54116ae
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-02-23 14:10:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -208,7 +208,8 @@ st.markdown(f"""
             --text-color: {theme['text']};
             --accent-color: {theme['accent']};
             --gradient: {theme['gradient']};
-            --sidebar-bg: #47B5FF;
+            --sidebar-bg: blue;
+            --sidebar-text-color: white;
             --card-bg: {'#F8F9FA' if is_light_theme else '#1E1E1E'};
             --card-hover-bg: {'#E9ECEF' if is_light_theme else '#2E2E2E'};
             --input-bg: {'#F8F9FA' if is_light_theme else '#1E1E1E'};
